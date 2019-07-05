0daac5b48486726090a7adb8dad791b12c29b614
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-07-05 23:45:09 +0600

    Created tuition app and relevant models

--- tuition/admin.py
+++ tuition/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ad, Question, Answer, Proposal, Assignee
 
-# Register your models here.
+
+admin.site.register(Ad)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Proposal)
+admin.site.register(Assignee)
