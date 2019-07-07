35179fe06377414ed929d3e069bcfa1e6a03b0ce
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-07-07 16:01:06 +0600

    Fully implemented home app

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-page')
+    path('', views.home, name='home-page'),
+    path('login/', views.login, name='login-page'),
+    path('register/', views.register, name='register-action'),
 ]
