cf99f7aae1c1f2ba93ecb367aca19a47ddd735fd
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-07-07 10:03:32 +0600

    Added __str__ to models

--- client/models.py
+++ client/models.py
@@ -6,3 +6,6 @@ class ClientFeedback(models.Model):
     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
     rating = models.IntegerField(verbose_name='Rating')
     feedback = models.TextField(verbose_name='Feedback')
+
+    def __str__(self):
+        return self.ad.title
