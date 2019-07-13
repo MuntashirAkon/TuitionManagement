7f547ea324e23e6342d6943967c210c18bb8b6e2
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-07-13 17:35:56 +0600

    Added required routing for tutor app

--- tutor/urls.py
+++ tutor/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='tutor-home')
+    path('', views.feed, name='tutor-home'),
+    path('feed/', views.feed, name='tutor-feed'),
+    path('apply/<int:ad_id>/', views.apply, name='tutor-apply'),
+    path('profile/<int:profile_id>/', views.view_profile, name='tutor-profile'),
+    path('profile/<int:profile_id>/edit', views.edit_profile, name='tutor-edit-profile'),
+    path('history/', views.history, name='tutor-history'),
+    path('settings/', views.settings, name='tutor-settings'),
+    path('logout/', views.logout, name='tutor-logout'),
+    path('login/', views.login, name='tutor-login'),
 ]
