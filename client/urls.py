cb9355f9302204331caa3d379012da8d05465c78
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-07-23 16:06:53 +0600

    Added client urls

--- client/urls.py
+++ client/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='client-home')
+    path('', views.home, name='client-home'),
+    path('login/', views.login, name='client-login'),
+    path('logout/', views.logout, name='client-logout'),
+    path('profile/<int:profile_id>/', views.view_profile, name='client-profile'),
+    path('ad/new/', views.new, name='client-new'),
+    path('ad/<int:add_id>/', views.view_applicants, name='client-applicants'),
+    path('running/', views.running, name='client-running'),
+    path('history/', views.history, name='client-history'),
+    path('settings/', views.settings, name='client-settings'),
 ]
